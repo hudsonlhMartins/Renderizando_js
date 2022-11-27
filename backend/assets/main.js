14e2b72ce564d241466393b1639e764c28ce2b36
hudson martins <hudsonlh.martins@gmail.com>
2022-11-26 22:38:29 -0300

    renderizacao pelo front com string

--- backend/assets/main.js
+++ backend/assets/main.js
@@ -1,1 +1,31 @@
-console.log('euu')
+import data from '/data.js'
+
+const cartapio = (menu)=>{
+    console.log("menu ==> ", menu)
+    return `
+    <div id=${menu.id} class="cartapio">
+        <header>
+            <h3>${menu.title} - ${data.restaurantes.get(menu.restaurantId).name}</h3>
+        </header>
+        <div class="cartapio_body">
+            <ul>
+                ${menu.sections.map(section =>(
+                    `
+                    <li>${section.title}</li>
+                        
+                    `
+                )).join("")}
+        
+            </ul>
+        </div>
+    </div>
+    `
+}
+
+const carapios = () =>{
+    return Array.from(data.menus.values()).slice(3)
+    .map(cartapio).join("")
+}
+
+const containerEl = document.querySelector(".cartapios_container")
+containerEl.insertAdjacentHTML("beforeend", carapios())
